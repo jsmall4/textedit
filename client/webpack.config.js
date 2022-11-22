1d95051b7b6e3e22bb329ab0c2e7e1e5d676f4f6
jsmall4 <64646369+jsmall4@users.noreply.github.com>
2022-11-22 16:23:18 +0000

    fixed install bug

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -36,8 +36,8 @@ module.exports = () => {
         description: "Keep on top of your notes!",
         background_color: "#225ca3",
         theme_color: "#225ca3",
-        start_url: "./",
-        publicPath: "./",
+        start_url: "/",
+        publicPath: "/",
         icons: [
           {
             src: path.resolve("src/images/logo.png"),
